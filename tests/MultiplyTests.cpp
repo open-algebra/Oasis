76ac299a7254bfc7ea0be0f5631623d29aa41765
Matthew McCall <m-mccall+git@outlook.com>
2023-12-01 17:03:45 -0500

    Add operator overloads

--- tests/MultiplyTests.cpp
+++ tests/MultiplyTests.cpp
@@ -86,3 +86,16 @@ TEST_CASE("Imaginary Multiplication", "[Imaginary][Multiplication]")
     REQUIRE(Oasis::Real { -1.0 }.Equals(*simplified2));
     REQUIRE(Oasis::Real { 1.0 }.Equals(*simplified4));
 }
+
+TEST_CASE("Multiply Operator Overload", "[Multiply][Operator Overload]")
+{
+    const std::unique_ptr<Oasis::Expression> a = std::make_unique<Oasis::Real>(1.0);
+    const std::unique_ptr<Oasis::Expression> b = std::make_unique<Oasis::Real>(2.0);
+
+    const auto sum = a*b;
+    auto realSum = Oasis::Real::Specialize(*sum);
+
+    REQUIRE(realSum != nullptr);
+    REQUIRE(realSum->GetValue() == 2.0);
+
+}
