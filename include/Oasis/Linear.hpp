c861073e5c0c846c6bbee6ad536f53eb9d1ac187
Andrew <andrew@thenazareths.net>
2024-03-22 17:33:42 -0400

    Added a function to search for (and potentially insert) a key. Continued the linear solve function.

--- include/Oasis/Linear.hpp
+++ include/Oasis/Linear.hpp
@@ -15,21 +15,47 @@
 
 namespace Oasis {
 
-typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatrixXXF;
-typedef Eigen::Matrix<float, Eigen::Dynamic, 1> Matrix1F;
+typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatrixXXD;
+typedef Eigen::Matrix<double, Eigen::Dynamic, 1> Matrix1D;
 
 /**
  * @param exprs A vector of expressions
  * @return map of variable to their values
  */
-auto SolveLinearSystems(std::vector<std::unique_ptr<Expression>>& exprs) -> std::map<std::string, float>;
+auto SolveLinearSystems(std::vector<std::unique_ptr<Expression>>& exprs) -> std::map<std::string, double>;
 
 /**
  *
  * @param exprs A vector of expressions
  * @return Dynamic Float matrix with the provided expressions inserted
  */
-auto ConstructMatrix(const std::vector<std::unique_ptr<Expression>>& exprs) -> std::pair<MatrixXXF, Matrix1F>;
+auto ConstructMatrices(const std::vector<std::unique_ptr<Expression>>& exprs)
+    -> std::pair<std::pair<MatrixXXD, Matrix1D>, std::map<std::string, size_t>>;
+
+/**
+ *
+ * @tparam u key type
+ * @tparam v value type
+ * @param map A map that maps some key to a value
+ * @param key The key you are searching for
+ * @param nextValue What the next assigned value should be assuming that the key doesn't already have one. If no value
+ * is found, this value will be assigned to this key.
+ * @return returns the value that is associated with the key and a boolean to say whether the nextValue was used.
+ */
+template <class u, class v>
+auto GetMapValue(std::map<u, v>& map, u key, v nextValue) -> std::pair<v, bool>
+{
+    v value;
+    bool usedNextValue = false;
+    if (auto p = map.find(key); p != map.end()) { // key is found
+        value = p->second;
+    } else { // key is not found
+        map.insert(std::make_pair(key, nextValue));
+        value = nextValue;
+        usedNextValue = true;
+    }
+    return std::make_pair(value, usedNextValue);
+}
 
 }
 
--- src/Linear.cpp
+++ src/Linear.cpp
@@ -5,42 +5,68 @@
 #include "Oasis/Linear.hpp"
 #include "Oasis/Add.hpp"
 #include "Oasis/Multiply.hpp"
+#include "Oasis/Variable.hpp"
 #include "iostream"
 
 namespace Oasis {
 
-auto SolveLinearSystems(std::vector<std::unique_ptr<Expression>>& exprs) -> std::map<std::string, float>
+auto SolveLinearSystems(std::vector<std::unique_ptr<Expression>>& exprs) -> std::map<std::string, double>
 {
     for (auto& expr : exprs) {
         expr = expr->Simplify();
     }
-    auto matrices = ConstructMatrix(exprs);
-    auto A = matrices.first;
-    auto b = matrices.second;
+    auto matrices = ConstructMatrices(exprs);
+    auto A = matrices.first.first;
+    auto b = matrices.first.second;
+    auto varMap = matrices.second;
 
-    return {};
+    // get result vector from A^-1*b=x
+    Matrix1D x = A.inverse() * b;
+
+    std::map<std::string, double> values;
+    for (auto& kv : varMap) {
+        auto key = kv.first;
+        auto index = kv.second;
+
+        values.insert(std::make_pair(key, x[index]));
+    }
+
+    return values;
 }
 
-auto ConstructMatrix(const std::vector<std::unique_ptr<Expression>>& exprs) -> std::pair<MatrixXXF, Matrix1F>
+auto ConstructMatrices(const std::vector<std::unique_ptr<Expression>>& exprs)
+    -> std::pair<std::pair<MatrixXXD, Matrix1D>, std::map<std::string, size_t>>
 {
     size_t numRows = exprs.size();
     size_t numCols = exprs.size();
 
-    MatrixXXF A;
-    Matrix1F b;
-
+    MatrixXXD A(numRows, numCols);
+    Matrix1D b(numRows);
+    size_t varCount = 0;
     std::map<std::string, size_t> vars; // variable name, column in matrix
     for (size_t row = 0; row < exprs.size(); row++) {
         std::vector<std::unique_ptr<Expression>> terms;
         auto expr = Add<Expression>::Specialize(*exprs[row]);
         expr->Flatten(terms);
         for (auto& term : terms) {
-            if (auto t = Real::Specialize(*term); t != nullptr) {
+            if (auto r = Real::Specialize(*term); r != nullptr) { // real number
+                b[row] = -1 * r->GetValue();
+            } else if (auto v = Variable::Specialize(*term); v != nullptr) { // variable by itself (coefficient of 1)
+                std::pair<size_t, bool> keyloc = GetMapValue<std::string, size_t>(vars, v->GetName(), varCount);
+                if (keyloc.second)
+                    varCount++;
+                A(row, keyloc.first) = 1;
+            } else if (auto exprV = Multiply<Real, Variable>::Specialize(*term); exprV != nullptr) {
+                // any expression times a variable
+                std::pair<size_t, bool> keyloc = GetMapValue<std::string, size_t>(vars, exprV->GetLeastSigOp().GetName(), varCount);
+                if (keyloc.second)
+                    varCount++;
+                A(row, keyloc.first) = exprV->GetMostSigOp().GetValue();
             }
         }
     }
 
-    return std::make_pair(A, b);
+    return std::make_pair(std::make_pair(A, b), vars);
 }
 
 }
