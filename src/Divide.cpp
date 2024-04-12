6b06d731957558d52d76865fd38208623fdc610c
Aditya Bachina <aditya.bachina@gmail.com>
2024-04-11 21:27:23 -0400

    all new tests finished

--- src/Divide.cpp
+++ src/Divide.cpp
@@ -303,13 +303,26 @@ auto Divide<Expression>::Differentiate(const Oasis::Expression & differentiation
         auto simplifiedDiv = this->Simplify();
 
         // Constant case - differentiation over a divisor
-        if (auto constant = Divide<Real, Expression>::Specialize(*simplifiedDiv); constant != nullptr) {
-            auto exp = constant->GetLeastSigOp().Copy();
-            auto num = constant->GetMostSigOp();
+        if (auto constant = Divide<Expression, Real>::Specialize(*simplifiedDiv); constant != nullptr) {
+            auto exp = constant->GetMostSigOp().Copy();
+            auto num = constant->GetLeastSigOp();
             auto differentiate = (*exp).Differentiate(differentiationVariable);
             if (auto add = Expression::Specialize(*differentiate); add != nullptr)
             {
                 return std::make_unique<Divide<Expression, Real>>(Divide<Expression, Real> {*(add->Simplify()), Real { num.GetValue() } })->Simplify();
+            }
+
+        }
+        //In case of simplify turning divide into mult
+        if (auto constant = Multiply<Expression, Real>::Specialize(*simplifiedDiv); constant != nullptr) {
+            std::cout << "THERE!" << std::endl;
+            auto exp = constant->GetMostSigOp().Copy();
+            auto num = constant->GetLeastSigOp();
+            auto differentiate = (*exp).Differentiate(differentiationVariable);
+            std::cout << "diff: " << differentiate->ToString() << std::endl;
+            if (auto add = Expression::Specialize(*differentiate); add != nullptr)
+            {
+                return std::make_unique<Multiply<Expression, Real>>(Multiply<Expression, Real> {*(add->Simplify()), Real { num.GetValue() } })->Simplify();
             }
 
         }
