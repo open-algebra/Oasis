75f8df5dc6cf7314c0b17ff01912004303847f68
Matthew McCall <m-mccall+git@outlook.com>
2024-04-23 01:05:13 -0400

    Refactor ToString method in Real class
    Updated the ToString() function in the Real class to use the fmt::format library over std::to_string. This change ensures that Real's value is represented with more precision.

--- src/Real.cpp
+++ src/Real.cpp
@@ -6,6 +6,8 @@
 
 #include "Oasis/Real.hpp"
 
+#include <fmt/format.h>
+
 namespace Oasis {
 
 Real::Real(double value)
@@ -30,7 +32,7 @@ auto Real::GetValue() const -> double
 
 auto Real::ToString() const -> std::string
 {
-    return std::to_string(value);
+    return fmt::format("{:.5}", value);
 }
 
 auto Real::ToMathMLElement(tinyxml2::XMLDocument& doc) const -> tinyxml2::XMLElement*
