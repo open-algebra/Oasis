81da643b6f17830fb3f574876be5f73b36237511
Matthew McCall <m-mccall+git@outlook.com>
2024-04-30 13:08:57 -0400

    Created new UnaryExpressionTests file and updated CMakeLists
    Created a new file, UnaryExpressionTests.cpp, for handling unary expression specific tests. The test case 'Substitute Unary' was moved from BinaryExpressionTests.cpp into this. Updated CMakeLists.txt to include UnaryExpressionTests.cpp during compilation.

---
+++ tests/UnaryExpressionTests.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Matthew McCall on 4/30/24.
+//
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <Oasis/Add.hpp>
+#include <Oasis/Multiply.hpp>
+#include <Oasis/Negate.hpp>
+#include <Oasis/Variable.hpp>
+
+TEST_CASE("Substitute Unary", "[Substitute]")
+{
+    Oasis::Add before {
+        Oasis::Multiply {
+            Oasis::Real { 2.0 },
+            Oasis::Negate { Oasis::Variable { "x" } } },
+        Oasis::Multiply{
+            Oasis::Real { 3.0 },
+            Oasis::Variable { "x" } }
+    }; // 2x+3x
+
+    auto after = before.Substitute(Oasis::Variable { "x" }, Oasis::Real { 4.0 }); // after should some std::unique_ptr<Expression> such that it equals 2(4) + 3(4)
+    Oasis::Real four { 4 };
+    REQUIRE(after->Equals(*(four.Simplify())));
+}
