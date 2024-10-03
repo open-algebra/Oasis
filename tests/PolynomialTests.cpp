afe6d4ce9bf3ed1770f29b1f93a564677547abcb
Matthew McCall <m-mccall+git@outlook.com>
2024-10-03 17:10:49 -0400

    Update PolynomialTests.cpp

--- tests/PolynomialTests.cpp
+++ tests/PolynomialTests.cpp
@@ -43,7 +43,7 @@ TEST_CASE("7th degree polynomial with rational roots", "[factor][duplicateRoot]"
     for (auto& i : zeros) {
         auto divideCase = Oasis::Divide<Oasis::Real>::Specialize(*i);
         REQUIRE(divideCase != nullptr);
-        std::tuple<long, long> asTuple = std::tuple(lround(divideCase->GetMostSigOp().GetValue()), lround(divideCase->GetLeastSigOp().GetValue()));
+        std::tuple<long, long> asTuple = std::tuple(lround(divideCase->GetMostSigOp().GetValue()), std::lround(divideCase->GetLeastSigOp().GetValue()));
         REQUIRE(goalSet.contains(asTuple));
         goalSet.erase(asTuple);
     }
