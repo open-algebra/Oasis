70438cb6d0ceadbfa2a121d79479f3fdab030575
Matthew McCall <m-mccall+git@outlook.com>
2025-01-31 21:13:24 -0500

    Refactor serializer usage for OASIS_CAPTURE_WITH_SERIALIZER.
    Replaced in-function serializer initialization with a global inline instance to streamline usage. Removed redundant includes, aligning headers with the updated macro definition. This simplifies the OASIS_CAPTURE_WITH_SERIALIZER macro and improves maintainability.

--- tests/AddTests.cpp
+++ tests/AddTests.cpp
@@ -12,7 +12,6 @@
 #include "Oasis/Real.hpp"
 #include "Oasis/RecursiveCast.hpp"
 #include "Oasis/Variable.hpp"
-#include "Oasis/InFixSerializer.hpp"
 
 TEST_CASE("Addition", "[Add]")
 {
@@ -23,8 +22,7 @@ TEST_CASE("Addition", "[Add]")
         Oasis::Real { 3.0 }
     };
 
-    Oasis::InFixSerializer serializer;
-    OASIS_CAPTURE_WITH_SERIALIZER(serializer, add);
+    OASIS_CAPTURE_WITH_SERIALIZER(add);
 
     auto simplified = add.Simplify();
     REQUIRE(simplified->Is<Oasis::Real>());
