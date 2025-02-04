cb89981cdde40a7789eec2786f949d45bea19a65
Sam-Anderson05 <samuelkevinanderson@gmail.com>
2025-02-04 16:09:58 -0500

    Changed to prevent segfault when dividing equal expressions

--- tests/DivideTests.cpp
+++ tests/DivideTests.cpp
@@ -4,13 +4,13 @@
 
 #include "catch2/catch_test_macros.hpp"
 
+#include "Oasis/Add.hpp"
 #include "Oasis/Divide.hpp"
+#include "Oasis/Exponent.hpp"
+#include "Oasis/Multiply.hpp"
 #include "Oasis/Real.hpp"
 #include "Oasis/RecursiveCast.hpp"
 #include "Oasis/Variable.hpp"
-#include "Oasis/Multiply.hpp"
-#include "Oasis/Exponent.hpp"
-#include "Oasis/Add.hpp"
 
 TEST_CASE("Division", "[Divide]")
 {
@@ -35,25 +35,22 @@ TEST_CASE("Symbolic Division, equal variables", "[Division][Symbolic]")
             Oasis::Real { 2.0 },
             Oasis::Variable { "x" } },
         Oasis::Multiply {
-                Oasis::Real { 1.0 },
+            Oasis::Real { 1.0 },
             Oasis::Variable { "x" } }
     };
-
 
     const Oasis::Divide div2 {
         Oasis::Multiply<> {
             Oasis::Real { 2.0 },
-            Oasis::Variable{ "z" },
-            Oasis::Variable{ "y" },
-            Oasis::Variable{ "x" }
-        },
+            Oasis::Variable { "z" },
+            Oasis::Variable { "y" },
+            Oasis::Variable { "x" } },
         Oasis::Multiply<> {
             Oasis::Real { 1.0 },
-            Oasis::Variable{ "y" },
-            Oasis::Variable{ "x" },
-            Oasis::Variable{ "z" }
-            }
-        };
+            Oasis::Variable { "y" },
+            Oasis::Variable { "x" },
+            Oasis::Variable { "z" } }
+    };
 
     auto simplified = div.Simplify();
     REQUIRE(simplified);
@@ -64,7 +61,6 @@ TEST_CASE("Symbolic Division, equal variables", "[Division][Symbolic]")
     REQUIRE(simplified2->Equals(Oasis::Real { 2.0 }));
 }
 
-
 TEST_CASE("Symbolic Division, unequal variables", "[Division][Symbolic]")
 {
     Oasis::Divide div {
@@ -72,60 +68,59 @@ TEST_CASE("Symbolic Division, unequal variables", "[Division][Symbolic]")
             Oasis::Real { 4.0 },
             Oasis::Multiply {
                 Oasis::Variable { "z" },
-                Oasis::Variable { "x" } }},
+                Oasis::Variable { "x" } } },
         Oasis::Multiply {
-                Oasis::Real { 2.0 },
-                Oasis::Multiply {
-                    Oasis::Variable { "y" },
-                    Oasis::Variable { "z" } }}
-        };
+            Oasis::Real { 2.0 },
+            Oasis::Multiply {
+                Oasis::Variable { "y" },
+                Oasis::Variable { "z" } } }
+    };
 
     auto simplified = div.Simplify();
 
     REQUIRE(Oasis::Divide {
         Oasis::Multiply {
             Oasis::Real { 2.0 },
-            Oasis::Variable { "x"} },
-        Oasis::Variable { "y" } }.Equals(*simplified));
+            Oasis::Variable { "x" } },
+        Oasis::Variable { "y" } }
+                .Equals(*simplified));
 }
-
 
 TEST_CASE("Symbolic Division of Expressions", "[Division][Symbolic]")
 {
-    //4(z^2+1)(x+1)/2(x+1)=2(z^2+1)
+    // 4(z^2+1)(x+1)/2(x+1)=2(z^2+1)
 
     Oasis::Divide div {
-        Oasis::Multiply{
+        Oasis::Multiply {
             Oasis::Multiply {
                 Oasis::Add {
                     Oasis::Variable { "x" },
                     Oasis::Real { 1 } },
-                Oasis::Add{
+                Oasis::Add {
                     Oasis::Exponent {
                         Oasis::Variable { "z" },
                         Oasis::Real { 2 } },
-                    Oasis::Real{ 1 }}},
-            Oasis::Real { 4.0 }
-        },
+                    Oasis::Real { 1 } } },
+            Oasis::Real { 4.0 } },
         Oasis::Multiply {
-                Oasis::Real { 2.0 },
-                Oasis::Add {
-                    Oasis::Variable { "x" },
-                    Oasis::Real { 1 } }}
-        };
+            Oasis::Real { 2.0 },
+            Oasis::Add {
+                Oasis::Variable { "x" },
+                Oasis::Real { 1 } } }
+    };
 
     auto simplified = div.Simplify();
 
     REQUIRE(Oasis::Multiply {
-        Oasis::Real{ 2 },
-        Oasis::Add{
+        Oasis::Real { 2 },
+        Oasis::Add {
             Oasis::Exponent {
                 Oasis::Variable { "z" },
                 Oasis::Real { 2 } },
-            Oasis::Real{ 1 }},
-        }.Equals(*simplified));
+            Oasis::Real { 1 } },
+    }
+                .Equals(*simplified));
 }
-
 
 TEST_CASE("Symbolic Division, unequal exponents", "[Division][Symbolic]")
 {
@@ -134,23 +129,23 @@ TEST_CASE("Symbolic Division, unequal exponents", "[Division][Symbolic]")
             Oasis::Real { 4.0 },
             Oasis::Exponent {
                 Oasis::Variable { "z" },
-                Oasis::Real { 2 } }},
+                Oasis::Real { 2 } } },
         Oasis::Multiply {
-                Oasis::Real { 2.0 },
-                Oasis::Multiply {
-                    Oasis::Variable { "y" },
-                    Oasis::Variable { "z" } }}
-        };
+            Oasis::Real { 2.0 },
+            Oasis::Multiply {
+                Oasis::Variable { "y" },
+                Oasis::Variable { "z" } } }
+    };
 
     auto simplified = div.Simplify();
 
     REQUIRE(Oasis::Divide {
         Oasis::Multiply {
             Oasis::Real { 2.0 },
-            Oasis::Variable { "z"} },
-        Oasis::Variable { "y" } }.Equals(*simplified));
+            Oasis::Variable { "z" } },
+        Oasis::Variable { "y" } }
+                .Equals(*simplified));
 }
-
 
 TEST_CASE("Symbolic Division, equal exponents", "[Division][Symbolic]")
 {
@@ -159,13 +154,13 @@ TEST_CASE("Symbolic Division, equal exponents", "[Division][Symbolic]")
             Oasis::Real { 4.0 },
             Oasis::Exponent {
                 Oasis::Variable { "z" },
-                Oasis::Real { 3 } }},
+                Oasis::Real { 3 } } },
         Oasis::Multiply {
-                Oasis::Real { 2.0 },
-                Oasis::Multiply {
-                    Oasis::Variable { "y" },
-                    Oasis::Variable { "z" } }}
-        };
+            Oasis::Real { 2.0 },
+            Oasis::Multiply {
+                Oasis::Variable { "y" },
+                Oasis::Variable { "z" } } }
+    };
 
     auto simplified = div.Simplify();
 
@@ -175,7 +170,17 @@ TEST_CASE("Symbolic Division, equal exponents", "[Division][Symbolic]")
             Oasis::Exponent {
                 Oasis::Variable { "z" },
                 Oasis::Real { 2 } } },
-        Oasis::Variable { "y" } }.Equals(*simplified));
+        Oasis::Variable { "y" } }
+                .Equals(*simplified));
+}
+
+TEST_CASE("Division of equal variables", "[Division][Symbolic]")
+{
+    // x/x = 1
+
+    Oasis::Divide div { Oasis::Variable { "x" }, Oasis::Variable { "x" } };
+    auto simplified = div.Simplify();
+    REQUIRE(Oasis::Real { 1 }.Equals(*simplified));
 }
 
 TEST_CASE("Generalized Division", "[Divide][Generalized]")
@@ -199,10 +204,9 @@ TEST_CASE("Divide Operator Overload", "[Divide][Operator Overload]")
     const std::unique_ptr<Oasis::Expression> a = std::make_unique<Oasis::Real>(1.0);
     const std::unique_ptr<Oasis::Expression> b = std::make_unique<Oasis::Real>(2.0);
 
-    const auto sum = b/a;
+    const auto sum = b / a;
     auto realSum = Oasis::RecursiveCast<Oasis::Real>(*sum);
 
     REQUIRE(realSum != nullptr);
     REQUIRE(realSum->GetValue() == 2.0);
-
 }
