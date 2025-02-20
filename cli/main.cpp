b2d5a454c1d75cbe36ae69f80df259fedd691f08
Matthew McCall <m-mccall+git@outlook.com>
2025-02-19 22:44:53 -0500

    Add CLI support with Linenoise and fmt integration
    This commit introduces a CLI tool for Oasis, leveraging Linenoise for input handling and fmt for output formatting. Changes include modifying CMakeLists.txt to conditionally build the CLI, adding FetchLinenoise.cmake, and creating the CLI implementation in cli/main.cpp. This enables a user-friendly interface for parsing and simplifying expressions interactively.

---
+++ cli/main.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Matthew McCall on 2/19/25.
+//
+
+#include <cstdlib>
+#include <cstdio>
+
+#include "Oasis/FromString.hpp"
+#include "Oasis/InFixSerializer.hpp"
+
+#include <fmt/color.h>
+#include <linenoise.h>
+
+int main(int argc, char **argv) {
+    std::setvbuf(stdout, nullptr, _IONBF, 0);
+    linenoiseHistorySetMaxLen(16);
+
+    Oasis::InFixSerializer serializer;
+
+    const char *line;
+    while((line = linenoise("> ")) != nullptr) {
+        std::string input { line };
+
+        linenoiseHistoryAdd(line);
+        delete[] line;
+
+        auto parseResult = Oasis::FromInFix(Oasis::PreProcessInFix(input));
+        auto err_style = fg(fmt::color::red);
+        if (!parseResult.Ok()) {
+            print(err_style, "< Failed to parse: {}\n", parseResult.GetErrorMessage());
+            continue;
+        }
+
+        auto simplifyResult = parseResult.GetResult().Simplify();
+        if (!simplifyResult) {
+            print(err_style, "< Failed to simplify\n");
+            continue;
+        }
+
+        auto serializeResult = simplifyResult->Accept(serializer);
+        if (!serializeResult) {
+            print(err_style, "< Failed to serialize\n");
+            continue;
+        }
+
+        fmt::println("< {}", serializeResult.value());
+    }
+
+    return EXIT_SUCCESS;
+}
