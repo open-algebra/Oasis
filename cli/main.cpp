f7090fb6652721e66c8ba0b47bee299cafb4605c
Matthew McCall <m-mccall+git@outlook.com>
2025-02-24 11:20:23 -0500

    Refactor input processing and add pipeline operator.
    Introduce a pipeline operator for cleaner chaining and simplify input handling logic in the REPL. Added error styling for better readability and resolved function template limitations with a wrapper.

--- cli/main.cpp
+++ cli/main.cpp
@@ -8,40 +8,45 @@
 #include "Oasis/FromString.hpp"
 #include "Oasis/InFixSerializer.hpp"
 
+#include <boost/callable_traits/return_type.hpp>
 #include <fmt/color.h>
 #include <linenoise.h>
 
-int main(int argc, char **argv) {
-    std::setvbuf(stdout, nullptr, _IONBF, 0);
+template <typename FnT>
+auto operator|(const std::string& str, FnT fn) -> boost::callable_traits::return_type_t<FnT>
+{
+    return fn(str);
+}
+
+int main(int argc, char** argv)
+{
     linenoiseHistorySetMaxLen(16);
 
     Oasis::InFixSerializer serializer;
+    auto err_style = fg(fmt::color::indian_red);
 
-    const char *line;
-    while((line = linenoise("> ")) != nullptr) {
-        std::string input { line };
+    const char* line;
+    while ((line = linenoise("> ")) != nullptr) {
+        std::string input{ line };
         delete[] line;
-
-        if (input.empty()) {
-            continue;
-        }
+        if (input.empty())  continue;
 
         linenoiseHistoryAdd(input.c_str());
 
-        auto parseResult = Oasis::FromInFix(Oasis::PreProcessInFix(input));
+        // Calling Oasis::FromInFix passed as template fails because defaulted parameters aren't represented in the type, so a wrapper is needed
+        auto Parse = [](const std::string& in) { return Oasis::FromInFix(in); };
+        auto result = (input | Oasis::PreProcessInFix | Parse)
+            .transform([](const std::unique_ptr<Oasis::Expression>& expr) -> std::unique_ptr<Oasis::Expression> {
+                return expr->Simplify();
+            })
+            .and_then([&serializer](const std::unique_ptr<Oasis::Expression>& expr) -> std::expected<std::string, std::string> {
+                return expr->Accept(serializer);
+            });
 
-        Oasis::FromInFix(Oasis::PreProcessInFix(input))
-        .and_then([&serializer](const std::unique_ptr<Oasis::Expression>& expr) -> std::expected<std::string, std::string> {
-            return expr->Accept(serializer);
-        })
-        .transform([&](const std::string& str) -> void {
-            fmt::println("  {}", str);
-        })
-        .transform_error([&](const std::string& err) -> std::string {
-            static auto err_style = fg(fmt::color::indian_red);
-            print(err_style, "  {}\n", err);
-            return err;
-        });
+        if (result.has_value()) fmt::println("  {}", result.value());
+        else fmt::println("  {}", styled(result.error(), err_style));
+
+        std::fflush(stdout);
     }
 
     return EXIT_SUCCESS;
