a1e817db4a13c4ad5d20363998e85c10f289269c
Jary Chen <jarychenu24@gmail.com>
2025-03-04 00:56:02 -0500

    Introduction of UnitGraph
    Introduce the UnitGraph.hpp for more robust and complex conversion
    instead of the manual conversion conditions. This will be the template
    hpp. Building the chain of equation conversion using Graph Algo.
    
    Signed-off-by: Jary Chen <jarychenu24@gmail.com>

---
+++ include/Oasis/UnitGraph.hpp
@@ -0,0 +1,41 @@
+//
+// Created by Jary Chen on 3/4/25.
+//
+
+#ifndef OASIS_UNIT_GRAPH_HPP
+#define OASIS_UNIT_GRAPH_HPP
+
+#include "Unit.hpp"
+#include <boost/graph/adjacency_list.hpp>
+#include <optional>
+#include <unordered_map>
+
+namespace Oasis {
+
+struct EdgeProperties {
+    double factor;
+};
+
+using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+                                    boost::no_property, EdgeProperties>;
+
+class UnitGraph {
+public:
+    UnitGraph();
+
+    void addConversion(Unit from, Unit to, double factor);
+
+    std::optional<double> findConversionFactor(Unit from, Unit to) const;
+
+private:
+    Graph graph_;
+    std::unordered_map<Unit, int> unitToVertex_;
+    int nextVertex_;
+
+    int getOrAddVertex(Unit unit);
+};
+
+} // namespace Oasis
+
+#endif // OASIS_UNIT_GRAPH_HPP
+
