7ee31aebabc0414430e01b24490c2b0c6f29ca03
Matthew McCall <m-mccall+git@outlook.com>
2025-03-11 16:32:46 -0400

    Update Boost download format to .tar.xz
    Switched the Boost archive format from .zip to .tar.xz for fetching. This ensures compatibility with the updated file provided by Boost and aligns with the correct SHA256 checksum. No functional changes to the build system were made.

--- cmake/FetchBoost.cmake
+++ cmake/FetchBoost.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
         Boost
-        URL "https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.zip"
-        URL_HASH SHA256=03530dec778bc1b85b070f0b077f3b01fd417133509bb19fe7c142e47777a87b
+        URL "https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.tar.xz"
+        URL_HASH SHA256=7da75f171837577a52bbf217e17f8ea576c7c246e4594d617bfde7fafd408be5
         DOWNLOAD_EXTRACT_TIMESTAMP ON
         EXCLUDE_FROM_ALL
 )
