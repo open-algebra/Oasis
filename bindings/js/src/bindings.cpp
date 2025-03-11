b309165f68d8e029341d87c607b30bb0fedcc76e
Matthew McCall <m-mccall+git@outlook.com>
2025-03-11 14:59:46 -0400

    Add WebAssembly build and JS bindings for Oasis
    This commit introduces support for compiling Oasis into WebAssembly using Emscripten, enabled by the `OASIS_BUILD_JS` flag. It includes CMake changes, JavaScript bindings, a test framework, TypeScript configuration, and necessary dependencies. Additionally, `.gitignore` and packaging files have been updated to accommodate the new build and runtime requirements.

---
+++ bindings/js/src/bindings.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Matthew McCall on 3/10/25.
+//
+#include <emscripten/bind.h>
+
+#include "Oasis/Expression.hpp"
+#include "Oasis/FromString.hpp"
+#include "Oasis/MathMLSerializer.hpp"
+
+namespace
+{
+    auto Simplify(std::shared_ptr<Oasis::Expression> expression_ptr) -> std::shared_ptr<Oasis::Expression>
+    {
+        return expression_ptr->Simplify();
+    }
+
+    auto FromInFixTrivial(const std::string& str) -> std::shared_ptr<Oasis::Expression>
+    {
+        return Oasis::FromInFix(str).value_or(nullptr);
+    }
+
+    auto ToMathMLString(std::shared_ptr<Oasis::Expression> expression_ptr) -> std::string
+    {
+        const auto& expression = *expression_ptr;
+
+        tinyxml2::XMLDocument doc;
+        Oasis::MathMLSerializer serializer(doc);
+        doc.InsertEndChild(expression.Accept(serializer).value());
+
+        tinyxml2::XMLPrinter printer;
+        doc.Print(&printer);
+
+        return{ printer.CStr() };
+    }
+}
+
+EMSCRIPTEN_BINDINGS(Oasis)
+{
+    emscripten::class_<Oasis::Expression>("Expression")
+        .smart_ptr<std::shared_ptr<Oasis::Expression>>("Expression");
+
+    emscripten::function("Simplify", &Simplify);
+
+    emscripten::function("PreProcessInFix", &Oasis::PreProcessInFix);
+    emscripten::function("FromInFix", &FromInFixTrivial);
+
+    emscripten::function("ToMathMLString", &ToMathMLString);
+}
---
+++ bindings/js/tests/index.ts
@@ -0,0 +1,11 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import loadOasis from "../../../cmake-build-debug-emscripten/bindings/js/oasis"
+
+test("Addition of Real Numbers", async () => {
+    const Oasis = await loadOasis();
+    const add = Oasis.FromInFix("2 + 2");
+    const addResult = Oasis.Simplify(add);
+    const addResultStr = Oasis.ToMathMLString(addResult);
+    assert.equal(addResultStr, "<mn>4</mn>\n");
+});
