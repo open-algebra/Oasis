12290a48c234a6df83797b590dd575338d6a7296
Sam-Anderson05 <samuelkevinanderson@gmail.com>
2025-04-01 16:43:47 -0400

    Added integral and derivative of e

--- src/EulerNumber.cpp
+++ src/EulerNumber.cpp
@@ -3,6 +3,11 @@
 //
 
 #include "Oasis/EulerNumber.hpp"
+#include "Oasis/Add.hpp"
+#include "Oasis/Integral.hpp"
+#include "Oasis/Multiply.hpp"
+#include "Oasis/Real.hpp"
+#include "Oasis/Variable.hpp"
 #include "numbers"
 #include "string"
 #include <cmath>
@@ -17,6 +22,27 @@ auto EulerNumber::Equals(const Expression& other) const -> bool
 auto EulerNumber::GetValue() -> double
 {
     return std::numbers::e;
+}
+
+auto EulerNumber::Differentiate(const Expression&) const -> std::unique_ptr<Expression>
+{
+    return std::make_unique<Real>(0);
+}
+
+auto EulerNumber::Integrate(const Expression& integrationVariable) const -> std::unique_ptr<Expression>
+{
+    if (auto variable = RecursiveCast<Variable>(integrationVariable); variable != nullptr) {
+        // Constant rule
+        Add adder {
+            Multiply<EulerNumber, Variable> { EulerNumber {}, Variable { (*variable).GetName() } },
+            Variable { "C" }
+        };
+        return adder.Simplify();
+    }
+
+    Integral<Expression, Expression> integral { *(this->Copy()), *(integrationVariable.Copy()) };
+
+    return integral.Copy();
 }
 
 }
