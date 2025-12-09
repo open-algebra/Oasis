8f169feb7b9c42c0b8c5d3c771bd10e5cb785ad6
Matthew McCall <m-mccall+git@outlook.com>
2025-12-09 17:31:47 -0500

    Add forward declarations header and refactor includes
    Introduce `FwdDecls.hpp` to centralize class and template forward declarations in the Oasis namespace. Updated `Visit.hpp` to use `FwdDecls.hpp`, removing redundant declarations. Adjusted `CMakeLists.txt` to include the new header.

--- include/Oasis/Visit.hpp
+++ include/Oasis/Visit.hpp
@@ -5,47 +5,10 @@
 #ifndef OASIS_SERIALIZATION_HPP
 #define OASIS_SERIALIZATION_HPP
 
+#include "FwdDecls.hpp"
 #include "Expression.hpp"
 
 namespace Oasis {
-
-class Real;
-class Imaginary;
-class Matrix;
-class Variable;
-class Undefined;
-class EulerNumber;
-class Pi;
-
-template <IExpression, IExpression>
-class Add;
-
-template <IExpression MinuendT, IExpression SubtrahendT>
-class Subtract;
-
-template <IExpression MultiplicandT, IExpression MultiplierT>
-class Multiply;
-
-template <IExpression DividendT, IExpression DivisorT>
-class Divide;
-
-template <IExpression BaseT, IExpression PowerT>
-class Exponent;
-
-template <IExpression BaseT, IExpression ArgumentT>
-class Log;
-
-template <typename OperandT>
-class Negate;
-
-template <typename OperandT>
-class Magnitude;
-
-template <IExpression, IExpression>
-class Derivative;
-
-template <IExpression Integrand, IExpression Differential>
-class Integral;
 
 class Visitor {
 public:
