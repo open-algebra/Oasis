6f3190c2b6adb56ee58dfb640141b02573db8396
Djan Carbonell <dcarbonell08@gmail.com>
2025-12-09 17:05:56 -0500

    Documentation for Add

--- include/Oasis/Add.hpp
+++ include/Oasis/Add.hpp
@@ -32,9 +32,70 @@ public:
 
 /**
  * The Add expression adds two expressions together.
+ * The Add expression takes in two and only two objects 
  *
+ * 
+ * @section Parameters
  * @tparam AugendT The type of the expression to add be added to.
  * @tparam AddendT The type of the expression to add to the augend.
+ * 
+ * @section Examples
+ * 
+ * The Add expression can add a multitude of OASIS class types.
+ * 
+ * @subsection Adding two Real values together.
+ * @code
+ * Oasis::Add myAdditionSimple {
+ *      Oasis::Real{4},
+ *      Oasis::Real{4},
+ *  };
+ *  Oasis::InFixSerializer result;
+ *
+ *    auto resultant = myAdditionSimple.Simplify();
+ * 
+ *  std::println("Result of addition: {}", resultant->Accept(result).value());
+ *  // Will print 8
+ * @endcode
+ * 
+ * @subsection Adding a Real value and a Variable together.
+ * @code
+ * Oasis::Add myAddition {
+        Oasis::Variable{"x"},
+        Oasis::Real{4}
+    };
+    //Oasis::InFixSerializer result;
+
+    resultant = myAddition.Simplify();
+
+    std::println("Result of addition: {}", resultant->Accept(result).value());
+    // Will print x+4
+ * @endcode
+ *
+ * @subsection Adding two Variables together.
+ * @code
+ * Oasis::Add myAdditionX {
+        Oasis::Variable{"x"},
+        Oasis::Variable{"x"}
+    };
+
+    resultant = myAdditionX.Simplify();
+
+    std::println("Result of addition: {}", resultant->Accept(result).value());
+    // Will print 2*x
+ * @endcode
+ *
+ * or if they are not the same variable:
+ * @code
+ * Oasis::Add myAdditionXY {
+        Oasis::Variable{"x"},
+        Oasis::Variable{"y"}
+    };
+
+    resultant = myAdditionXY.Simplify();
+
+    std::println("Result of addition: {}", resultant->Accept(result).value());
+    // Will print x+y
+ * @endcode
  */
 template <IExpression AugendT = Expression, IExpression AddendT = AugendT>
 class Add : public BinaryExpression<Add, AugendT, AddendT> {
