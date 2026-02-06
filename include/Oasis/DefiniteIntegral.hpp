ee087634efe8ffe85fa7a92577dc061eb5fdde5e
JooseMann <justinkarate76@gmail.com>
2026-02-05 20:25:42 -0500

    Started work on definite integration, no tests written yet

--- include/Oasis/DefiniteIntegral.hpp
+++ include/Oasis/DefiniteIntegral.hpp
@@ -6,11 +6,21 @@
 #define DEFINITEINTEGRAL_HPP
 
 #include "BoundedBinaryExpression.hpp"
+#include "Oasis/Integral.hpp"
 
 namespace Oasis {
 
 template <IExpression OperandT, IExpression VariableT, IExpression LowerBoundT, IExpression UpperBoundT>
-class DefiniteIntegral final : BoundedBinaryExpression<DefiniteIntegral, OperandT, VariableT, LowerBoundT, UpperBoundT> { };
+class DefiniteIntegral : BoundedBinaryExpression<DefiniteIntegral, OperandT, VariableT, LowerBoundT, UpperBoundT> {
+public:
+    DefiniteIntegral(const Integral<Expression, Expression>& original);
+
+    auto IntegrateWithBounds(const VariableT& var) -> std::unique_ptr<Expression>;
+
+    auto AcceptInternal(const Visitor& visitor) -> any;
+private:
+    Integral<Expression, Expression> integral;
+};
 
 } // Oasis
 
