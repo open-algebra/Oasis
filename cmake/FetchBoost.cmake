cfe86d95d79e944439e54624c53e8a6943c4125d
Matthew McCall <m-mccall+git@outlook.com>
2026-03-10 16:36:36 -0400

    Update CMake FetchContent to use system installed Boost when available. (#204)
    * Refactor Boost integration in CMake configuration.
    
    Simplified and unified Boost usage by replacing specific libraries with `Boost::boost` target. Adjusted FetchContent calls to include `FIND_PACKAGE_ARGS` and enabled compatibility targets. Updated Catch2 FetchContent for consistency.
    
    * [Actions] Format CMakeLists.txt
    
    * Trigger workflow
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- cmake/FetchBoost.cmake
+++ cmake/FetchBoost.cmake
@@ -4,7 +4,9 @@ FetchContent_Declare(
         URL_HASH SHA256=7da75f171837577a52bbf217e17f8ea576c7c246e4594d617bfde7fafd408be5
         DOWNLOAD_EXTRACT_TIMESTAMP ON
         EXCLUDE_FROM_ALL
+        FIND_PACKAGE_ARGS CONFIG
 )
 
 set(BOOST_INCLUDE_LIBRARIES any callable_traits mpl)
+set(BOOST_ENABLE_COMPATIBILITY_TARGETS ON)
 FetchContent_MakeAvailable(Boost)
